144ae83bcd3b911c863986e5c0b017db28087faa
Anton Babenko <anton.nones88@gmail.com>
2023-04-24 13:47:44 +0400

    server2 try debug2

--- routers/PlayerRouter.js
+++ routers/PlayerRouter.js
@@ -5,8 +5,8 @@ const playerRouter = new Router()
 
 playerRouter.get('/', PlayerController.getAll)
 playerRouter.get('/:id', PlayerController.getOne)
-playerRouter.get('/', PlayerController.create)
-playerRouter.get('/', PlayerController.update)
-playerRouter.get('/:id', PlayerController.delete)
+playerRouter.post('/', PlayerController.create)
+playerRouter.put('/', PlayerController.update)
+playerRouter.delete('/:id', PlayerController.delete)
 
 export default playerRouter
