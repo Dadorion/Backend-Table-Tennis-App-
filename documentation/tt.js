a5b61532182575ee49cfbf9d0c1c45e38a73b685
Anton Babenko <anton.notes88@gmail.com>
2024-02-09 15:00:32 +0400

    удалил лишние директории

--- documentation/tt.js
+++
@@ -1,9 +0,0 @@
-let bool = false
-const value = '1982-04-23'
-const today = new Date();
-const newDate = new Date(value)
-if (new Date(value).getTime() < today.getTime()) { bool = true }
-
-console.log(newDate.getTime())
-console.log(today.getTime())
-console.log(bool)
